9710113d4a77cc7acb5c46c0d0ab67998cab9d77
Василий Романов <myhappy1988@gmail.com>
2022-12-15 16:34:58 +0300

    some update

--- routes.js
+++ routes.js
@@ -8,7 +8,7 @@ const classes = {
     route: 'route',
     activeUp: 'route--active-up',
     activeDown: 'route--active-down',
-    placeholder: 'placeHolder'
+    placeholder: 'placeholder'
 }
 
 class Routes {
@@ -22,12 +22,15 @@ class Routes {
         this.dragCurrentRoute = null;
         this.dropCurrentRoute = null;
 
-        this.startClick = null;
-        this.shiftX = null;
-        this.shiftY = null;
+        // // this.startClick.target = null;
+        // this.shiftX = null;
+        // this.shiftY = null;
 
-        // Ссылка, куда будем скидывать перетаскиваемый элемент
-        this.currentDroppAble = null;
+        
+        
+        // // Ссылка, куда будем скидывать перетаскиваемый элемент
+        // this.currentDroppAble = null;
+        // this.placeholderNode = null;
 
         // Bind functions
         this.manageHTML = this.manageHTML.bind(this)
@@ -37,10 +40,11 @@ class Routes {
         this.setParameters = this.setParameters.bind(this)
         this.changeRoutes = this.changeRoutes.bind(this)
         this.dragStart = this.dragStart.bind(this)
-        this.stopDrag = this.stopDrag.bind(this)
-        this.dragEnter = this.dragEnter.bind(this)
-        this.dragLeave = this.dragLeave.bind(this)
-        this.changeDragList = this.changeDragList.bind(this)
+        this.dropped = this.dropped.bind(this)
+        this.moveAt = this.moveAt.bind(this)
+        this.moving = this.moving.bind(this)
+        
+    
 
         // Run functions
         this.manageHTML()
@@ -80,9 +84,7 @@ class Routes {
     }
 
     setParameters() {
-        // Начало движения для tranform translateY
-        this.y = 0;
-        // this.routeNode = Array.from(this.routesNode.children);
+        
     }
 
     setEvents() {
@@ -90,9 +92,9 @@ class Routes {
         this.enterRouteNode.addEventListener('change', this.addRoute)
         this.routeNode = Array.from(this.routesNode.children)
         this.routeNode.map((route, index) => {
-            route.addEventListener('pointerdown', this.startDrag)
+            route.addEventListener('pointerdown', this.dragStart)
+            route.addEventListener('pointerup', this.dropped)
         })
-        window.addEventListener('pointerup', this.stopDrag)
     }
 
     // Render of the map on the page
@@ -134,26 +136,96 @@ class Routes {
         }
     }
 
-    // Event pointerdown
-    dragStart() {
+    dragStart(event) {
         this.startClick = event;
 
-        // Заменяем на тот, по которому кликнули
-        this.startClick.target.replaceWith(createPlaceholderNode);
+        this.placeholderNode = document.createElement('div')
+        this.placeholderNode.classList.add(classes.placeholder)
 
+        // Заменяем на тот, по которому кликнули
+        this.startClick.target.replaceWith(this.placeholderNode)
+
+        this.currentDroppAble = null
+        
         // Берем координаты Placeholder от верхнего левого угла
-        let coordinatesOfPlaceholder = document.elementFromPoint(event.pageX, event.pageY).getBoundingClientRect();
+        this.coordinatesOfPlaceholder = document.elementFromPoint(event.pageX, event.pageY).getBoundingClientRect()
 
         // Из расстояния от мыши до экрана (clientY/X)
         // Вычитаем координаты из Placeholder
         // Что бы курсор не сдвигался в центр перетаскиваемого элемента
-        shiftX = event.clientX - coordinatesOfPlaceholder.left;
-        shiftY = event.clientY - coordinatesOfPlaceholder.top;
+        this.shiftX = event.clientX - this.coordinatesOfPlaceholder.left
+        this.shiftY = event.clientY - this.coordinatesOfPlaceholder.top
 
         // Настраиваем движущийся элемент и добавляем в body
-        this.startClick.target.style.position = 'absolute';
-        this.startClick.target.style.zIndex = 1000;
-        document.body.append(startClick.target);
+        this.startClick.target.style.position = 'absolute'
+        this.startClick.target.style.zIndex = 1000
+
+        this.startClick.target.style 
+        document.body.append(this.startClick.target)
+
+        // Задаем координаты из Placehholder элементу, который двигаем
+        // Что бы его top и left не ушли в ноль, после смены position на absolute
+        this.startClick.target.style.left = `${this.coordinatesOfPlaceholder.x}px`
+        this.startClick.target.style.top = `${this.coordinatesOfPlaceholder.y}px`
+
+        console.log('Start --> ', event.target)
+
+        document.addEventListener('pointermove', this.moving)
+    }
+
+    // Ф-я двигает элемент
+    moveAt(pageX, pageY) {
+        this.startClick.target.style.left = `${pageX - this.shiftX}px`
+        this.startClick.target.style.top = `${pageY - this.shiftY}px`
+    }
+
+    // Ф-я для event pointermove, двигать наш элемент
+    moving(event) {
+        this.moveAt(event.pageX, event.pageY)
+        // Скрываем движущийся элемент, что бы получить координаты
+        // элемента над которым мы проходим
+        this.startClick.target.style.display = 'none'
+        
+        // получаем координаты
+        let elemBelow = document.elementFromPoint(event.clientX, event.clientY)
+
+        // Возращаем видимость элемента
+        this.startClick.target.style.display = 'flex'
+        this.startClick.target.hidden = false
+
+        // Если курсор за передлами окна
+        if (!elemBelow) return
+
+        // Получаем элемент на который будем переносить
+        let droppableBelow = elemBelow.closest('.route')
+
+        // Если мы еще не были над нужным элементом
+        if (this.currentDroppAble != droppableBelow) {
+            // Если уже были и ушли, удаляем действия
+            if (this.currentDroppAble) {
+                console.log('We leave')
+
+            }
+            // Если нашли нужный элемент, добавляем в currentDroppAble
+            this.currentDroppAble = droppableBelow
+            // Если только зашли на элемента для dropp
+            if (this.currentDroppAble) {
+                console.log('We came')
+
+            }
+        }
+    }
+
+    // Ф-я для event Pointerup
+    dropped() {
+        document.removeEventListener('pointermove', this.moving)
+        // Если не было dropp, возвращаем элемент на место
+        this.placeholderNode.replaceWith(this.startClick.target)
+        this.startClick.target.style.position = 'inherit'
+        this.startClick.target.style.top = '0'
+        this.startClick.target.style.left = '0'
+
+        this.placeholderNode.remove()
     }
 
 }
